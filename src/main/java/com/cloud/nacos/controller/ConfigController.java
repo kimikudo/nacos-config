073cf27618045f7f43c83a37f0a7c72d90388a2f
kimikudo <kudo-shinichi@live.cn>
2020-04-03 09:24:23 +0800

    add code to new branch

---
+++ src/main/java/com/cloud/nacos/controller/ConfigController.java
@@ -0,0 +1,20 @@
+package com.cloud.nacos.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+@RefreshScope//SpringCloud原生注解,配置自动更新
+public class ConfigController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
